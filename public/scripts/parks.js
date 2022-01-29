5a11c30a03aa3f61cffbcaa64573ede2af9a22b3
Scott Isitt <isitts@hotmail.com>
2022-01-29 00:15:00 -0800

    Incorporate parks.js in index.ejs so list  of parks show uo beneath the map.

--- public/scripts/parks.js
+++ public/scripts/parks.js
@@ -9,21 +9,6 @@ function fetchData() {
             return res.json()
         })
         .then(data => {
-            // console.log(data.features)
-            // const parkName = data.features[0].attributes.ParkName
-            // const features = data.features
-
-            // document.querySelector("#app")
-            //     .insertAdjacentHTML("afterbegin", `<h1>${parkName}</h1>`)
-
-            // My loop
-            // for (let i = 0; i < features.length; i++) {
-            //     const feature = data.features[i];
-            //     console.log(feature.attributes.ParkName)
-            //     const park = feature.attributes.ParkName
-            //     document.querySelector("#app")
-            //         .insertAdjacentHTML("afterbegin", `<h1>${park}</h1>`)
-            // }
 
             // Video 3's loop (.map) 
             const features = data.features.map(f => {
