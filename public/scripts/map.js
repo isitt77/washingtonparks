cc2328710ca659f1c53a0d7b09900ca022577ab2
Scott Isitt <isitts@hotmail.com>
2022-01-30 00:59:09 -0800

    Copy and paste parks.js content into map.js file, above the mapbox code.

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,3 +1,38 @@
+// Parks
+const ParksApi = "https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+
+function fetchData() {
+    fetch(ParksApi)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Response not ok.")
+            }
+            return res.json()
+        })
+        .then(data => {
+            // console.log(data)
+            // Video 3's loop (.map) 
+            const features = data.features.map(f => {
+                // console.log(f.attributes)
+                return `<p>${f.attributes.OBJECTID}. ${f.attributes.ParkName} ${f.geometry.rings[0]}</p>`
+            }).join(" ")
+            // console.log(features)
+            document.querySelector("#app")
+                .insertAdjacentHTML("afterbegin", features)
+            return features
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+fetchData()
+
+
+
+
+// MapBox
 mapboxgl.accessToken = "pk.eyJ1IjoiaXNpdHQ3NyIsImEiOiJja3dvam45NjAwM2oyMndqdjNzdHNpZGtmIn0.N1zn-7deVgeJLaqFawePDg";
 const map = new mapboxgl.Map({
     container: 'map', // container ID
