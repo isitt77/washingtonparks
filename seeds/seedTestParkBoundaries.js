f87967ab6065a49fc628015964111ffa1e78f5d7
Scott Isitt <isitts@hotmail.com>
2022-02-20 00:14:40 -0800

    Create seedTestParkBoundaries.js to prep for many park entries.

---
+++ seeds/seedTestParkBoundaries.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Park = require("../models/park");
+
+// mongoose.connect("mongodb://localhost:27017/stateParkSite", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/stateParkSite")
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"))
+db.once("open", () => {
+    console.log("parks database connected")
+})
+
+
+const seedDB = async () => {
+    await Park.deleteMany({})
+    // const park = new Park({
+       
+    // })
+    // await park.save()
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
