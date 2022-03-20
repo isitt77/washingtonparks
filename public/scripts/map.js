902a74b33911001a70cc06241d68d1632a93ce35
Scott Isitt <isitts@hotmail.com>
2022-03-19 18:37:20 -0700

    Create popupMarker branch and copy and paste code from Mapbox into map.js.

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -46,100 +46,38 @@ map.on('load', () => {
             // 'fill-opacity': 0.75,
             'line-color': '#008000',
             'line-width': 5
-            // 'circle-color': [
-            //     'step',
-            //     ['get', 'point_count'],
-            //     '#51bbd6',
-            //     100,
-            //     '#51bbd6',
-            //     750,
-            //     '#51bbd6'
-            // ],
-            // 'circle-radius': [
-            //     'step',
-            //     ['get', 'point_count'],
-            //     20,
-            //     10,
-            //     30,
-            //     20,
-            //     40
-            // ]
         }
     });
 
-    // map.addLayer({
-    //     id: 'cluster-count',
-    //     type: 'symbol',
-    //     source: 'parks',
-    //     filter: ['has', 'point_count'],
-    //     layout: {
-    //         'text-field': '{point_count_abbreviated}',
-    //         'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-    //         'text-size': 12
-    //     }
-    // });
+    // Create a popup, but don't add it to the map yet.
+    const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
 
-    // map.addLayer({
-    //     id: 'unclustered-point',
-    //     type: 'circle',
-    //     source: 'parks',
-    //     filter: ['!', ['has', 'point_count']],
-    //     paint: {
-    //         'circle-color': '#11b4da',
-    //         'circle-radius': 4,
-    //         'circle-stroke-width': 1,
-    //         'circle-stroke-color': '#fff'
-    //     }
-    // });
+    map.on('mouseenter', 'places', (e) => {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
 
-    // // inspect a cluster on click
-    // map.on('click', 'clusters', (e) => {
-    //     const features = map.queryRenderedFeatures(e.point, {
-    //         layers: ['clusters']
-    //     });
-    //     const clusterId = features[0].properties.cluster_id;
-    //     map.getSource('parks').getClusterExpansionZoom(
-    //         clusterId,
-    //         (err, zoom) => {
-    //             if (err) return;
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
 
-    //             map.easeTo({
-    //                 center: features[0].geometry.coordinates,
-    //                 zoom: zoom
-    //             });
-    //         }
-    //     );
-    // });
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
 
-    // // When a click event occurs on a feature in
-    // // the unclustered-point layer, open a popup at
-    // // the location of the feature, with
-    // // description HTML from its properties.
-    // map.on('click', 'unclustered-point', (e) => {
-    //     // popupMarkup is from a virtual in the campground schema (campground.js)
-    //     const { popupMarkup } = e.features[0].properties
-    //     const coordinates = e.features[0].geometry.coordinates.slice();
-    //     const mag = e.features[0].properties.mag;
-    //     const tsunami =
-    //         e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates).setHTML(description).addTo(map);
+    });
 
-    //     // Ensure that if the map is zoomed out such that
-    //     // multiple copies of the feature are visible, the
-    //     // popup appears over the copy being pointed to.
-    //     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    //         coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    //     }
-
-    //     new mapboxgl.Popup()
-    //         .setLngLat(coordinates)
-    //         .setHTML(popupMarkup)
-    //         .addTo(map);
-    // });
-
-    // map.on('mouseenter', 'clusters', () => {
-    //     map.getCanvas().style.cursor = 'pointer';
-    // });
-    // map.on('mouseleave', 'clusters', () => {
-    //     map.getCanvas().style.cursor = '';
-    // });
+    map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+    // End popupMarker paste
 });
